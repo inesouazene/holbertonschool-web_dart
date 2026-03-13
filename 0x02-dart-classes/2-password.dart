cfffb6c19b1c1d57cdee47f605cb8ec13b3aea79
inesouazene <inesouazene@gmail.com>
2026-03-13 11:12:52 +0100

    add file for task 2

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,31 @@
+/* Create a class called Password and give it a string property called password.
+
+* Add a method to Password called isValid that returns true only
+*   -  The length of password should be between 8 and 16.
+*   -  password should contains Uppercase letters and lowercase letters
+*   -  password should contains numbers .
+*   -  toString :
+*   -  Example password = "ShouldWorkf7ne"
+*   -  Override the toString method of Password class to prints Your Password is: ShouldWorkf7ne.
+
+*/
+
+class Password {
+  late String password;
+
+  bool isValid() {
+    if ((password.length < 8 || password.length > 16) || 
+        !password.contains(RegExp(r'[A-Z]')) &&
+        !password.contains(RegExp(r'[a-z]')) &&
+        !password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+    return true;  
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+
+}
