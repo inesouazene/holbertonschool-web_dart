48861c27d1a34c49f840f5002ef66b39873dcaae
inesouazene <inesouazene@gmail.com>
2026-03-13 10:29:35 +0100

    add file for task 0 and README.md

---
+++ 0x02-dart-classes/0-class.dart
@@ -0,0 +1,8 @@
+// Create a User class that contains a property called name and a method called showName that returns a string representation of User "Hello name"
+class User {
+  late String name;
+
+  String showName() {
+    return "Hello $name";
+  }
+}
