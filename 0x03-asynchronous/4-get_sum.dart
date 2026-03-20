f6e807b308a2f9eebcff391dddabdf62c7e5dcda
inesouazene <inesouazene@gmail.com>
2026-03-20 11:49:35 +0100

    add file for task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,22 @@
+import '4-util.dart';
+import 'dart:convert';
+
+// function calculateTotal() that calculates the total price of items for a certain user
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userDataMap = jsonDecode(userData);
+    String userId = userDataMap['id'];
+    String ordersData = await fetchUserOrders(userId);
+    List<dynamic> orders = jsonDecode(ordersData);
+    double totalPrice = 0.0;
+    for (var product in orders) {
+      String priceString = await fetchProductPrice(product);
+      double price = jsonDecode(priceString).toDouble();
+      totalPrice += price;
+    }
+    return totalPrice;
+  } catch (error) {
+    return -1;
+  }
+}
